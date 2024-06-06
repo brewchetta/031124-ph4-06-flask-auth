41978b0b40cb91f615a062dd501b62803231051c
brewchetta <chett.tiller@gmail.com>
2024-06-06 11:29:25 -0400

    yay delpyment

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <div className="App">
 
-      <h1>Authentication + Authorization</h1>
+      <h1>Yay Deployment!</h1>
 
       <UserPanel currentUser={currentUser} setCurrentUser={setCurrentUser} />
 
